d075c4771e68b9d2114156a6f4a6a5692a666b16
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-25 13:51:41 -0800

    User model and seed.js created

---
+++ seeds/seed.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData);
+
+  process.exit(0);
+};
+
+seedDatabase();
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -17,14 +16,18 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
-
 
 app.use(session(sess));
 
@@ -38,5 +41,5 @@ app.use(express.static('public'));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
 });
